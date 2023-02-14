f427dc95d5c512b61530e17627401c47bfe5f7b0
Cailan <cd2389rocks@gmail.com>
2023-02-14 12:05:35 -0500

    Shapes exercise 2/13

---
+++ ch02/exercises/shapes.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.init()
+while 1:
+    pygame.event.get()
+    screen=pygame.display.set_mode()
+    screen.fill("black")
+    pygame.draw.circle (screen, "blue", [300,200], 50)
+    pygame.draw.circle (screen, "blue", [300,150], 40)
+    pygame.draw.circle (screen, "blue", [300,100], 30)
+    pygame.display.flip()
+    pygame.time.wait (1000)
+    break
+
