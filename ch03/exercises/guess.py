82716af4d52a0108998ca9bf7a7c5f09428e4e5f
Cailan <cd2389rocks@gmail.com>
2023-02-22 16:49:20 -0500

    Guess activity update 2/22

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,7 +1,7 @@
 import random
 the_number=random.randrange(1,11)
 for i in range(1,4):
-    my_guess=int(input("Guess the number: "))
+    my_guess=int(input("Guess a number between 1 and 10: "))
     if my_guess<the_number:
         print("Too Low")
     elif my_guess>the_number:
