6066a8b462c4ca8b4aa0bcce8b3cf24ee50c5692
Cailan <cd2389rocks@gmail.com>
2023-03-06 19:06:41 -0500

    pattern activity

---
+++ ch05/exercises/patterns.py
@@ -0,0 +1,14 @@
+def star_pyramid():
+    rows=int(input("Enter the number of rows: "))
+    for i in range (1, rows+1):
+        print("*" * i)
+star_pyramid()
+
+def rstar_pyramid():
+    rows=int(input("Enter the number of rows: "))
+    for i in range (rows+1, 0, -1):
+        print("*" * i)
+rstar_pyramid()
+
+    
+  
