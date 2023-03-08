ae5d3b94b3336aeb4b999ff72203ad631618d5fe
Cailan <cd2389rocks@gmail.com>
2023-03-08 16:49:29 -0500

    star pyramid updated

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,13 +1,14 @@
 def star_pyramid():
-    rows=int(input("Enter the number of rows: "))
     for i in range (1, rows+1):
         print("*" * i)
-star_pyramid()
+
 
 def rstar_pyramid():
-    rows=int(input("Enter the number of rows: "))
     for i in range (rows, 0, -1):
         print("*" * i)
+
+rows=int(input("Enter the number of rows: "))
+star_pyramid()
 rstar_pyramid()
 
     
