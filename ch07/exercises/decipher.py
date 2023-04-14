cf68bbb2f3426c6da27c75b08d68d7e4c6846134
Cailan <cd2389rocks@gmail.com>
2023-04-14 12:02:57 -0400

    decipher updated

--- ch07/exercises/decipher.py
+++ ch07/exercises/decipher.py
@@ -1,27 +1,28 @@
 import json
-def decipher(text):
+def decipher(fptr, shift):
     """
-    This function is going to decrypt the text in thre encrypted-#B... file using the decipher technique
+    This function is going to decrypt the text in the encrypted-#B... file using the decipher technique
     args: 
         text: the text in the encrypted-#B... file (str)
-        shift: i
+        shift: 1
     return: the decrypted message (str)
     """
     result=""
-    for i in range (0,27):
-        sentence=str("The quick brown fox jumps over the lazy dog")
-        for char in text:
-            if char.isalpha():
-                start=ord('A') if char.isupper() else ord('a')
-                new_pos = (ord(char) - start + i) % 26
-                char = chr(start - new_pos)
+    for char in fptr:
+        if char=="W":
+            result+=" "
+        elif char=="1":
+            result+="T"
+        elif char.isalpha():
+            start=ord('A') if char.isupper() else ord('a')
+            new_pos = (ord(char) - start + shift) % 26
+            char = chr(start + new_pos)
             result+=char
-            if sentence==result:
-                break
-            return result
+    return result
 def main():
-    fptr = open("ch07/exercises/encrypted-#B00970858.txt", "r")
-    result=decipher(fptr)
+    text=str("1ifWrvjdlWcspxoWgpyWkvnqtWpwfsWuifWmbazWeph")
+    shift=-1
+    result=decipher(text, shift)
     fptr=open("ch07/exercises/decrypted.txt", "w")
     fptr.write(result)
     fptr.close()
